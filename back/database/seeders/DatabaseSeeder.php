a14e60222669799a9d721fcf9222cd74f45c5c98
Uxoa <palomita@gmail.com>
2023-03-05 13:58:00 +0100

    Deleting and tryin otherquestions that I have myself

--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -1,17 +1,22 @@
 <?php
 
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use App\Models\Item;
-use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
-    public function run()
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
     {
-        User::factory()->count(10)->create()->each(function ($user) {
-            Item::factory()->count(5)->create(['user_id' => $user->id]);
-        });
+        // \App\Models\User::factory(10)->create();
+
+        // \App\Models\User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
     }
 }
-
