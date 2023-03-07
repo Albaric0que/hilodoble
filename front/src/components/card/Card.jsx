2f41d25b710cc06053f12480f42d5a1bb37e060f
Elena <elenarjona@gmail.com>
2023-03-07 13:32:14 +0100

    card

---
+++ front/src/components/card/Card.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import '../card/Card.css'
+
+function Card() {
+  return (
+    <div className= "container">
+
+      <div className= "img-container">
+      </div>
+
+      <div>
+        <h3 className= "name-item">Riñonera de lona reciclada</h3>
+        <h3 className= "price-item">14,90€</h3>
+        <button className= "button">Añadir al carrito</button>
+      </div>
+
+    </div>
+  )
+}
+
+export default Card
