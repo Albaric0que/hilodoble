edbefd12bbbe72d944d66b28e6c37674eb37e5b4
Albaric0que <albamrus@gmail.com>
2023-03-07 17:26:05 +0100

    Create read in userCRUD

--- back/database/migrations/2014_10_12_000000_create_users_table.php
+++ back/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,17 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+
             $table->string('name');
+            $table->string('surname');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('phone');
+            $table->string('address');
+            $table->string('postcode');
+            $table->boolean('isAdmin')->default(false);
+            
             $table->rememberToken();
             $table->timestamps();
         });
--- back/database/seeders/DatabaseSeeder.php
+++ back/database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\User;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+
+
         // \App\Models\User::factory(10)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
         // ]);
+
+        User::factory()->create([
+         'name' => 'admin',
+         'email' => 'admin@admin.com',
+         'password' => 'password', // password
+         'phone' => '666123456',
+         'address' => 'Calle hilodoble, número 7',
+         'postcode' => '29010',
+         'isAdmin' => true
+        ]);
+
+        User::factory(5)->create();
     }
+
 }
