ae44014109c2cbd22cdb401fe8dc6b2d4c332d51
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-03-07 15:02:44 +0100

    footer component

---
+++ front/src/components/Footer/Footer.jsx
@@ -0,0 +1,39 @@
+import './Footer.css'
+import * as React from 'react';
+
+function Footer() {
+  
+  return (
+
+    <div className='footerContainer'>
+      <div className='footerTop'>
+        <div className='footerBlock'>
+
+        </div>
+        <div className='footerBlock'>
+          <ul>
+            <li><a href="#">macarrones</a></li>
+            <li><a href="#">macarrones</a></li>
+            <li><a href="#">macarrones</a></li>
+            <li><a href="#">macarrones</a></li>
+          </ul>
+        </div>
+        <div className='footerBlock'>
+
+        </div>
+      </div>
+      
+      <div className='subFooter'>
+        <ul>
+          <li><a href="#">macarrones</a></li>
+          <li><a href="#">macarrones</a></li>
+          <li><a href="#">macarrones</a></li>
+          <li><a href="#">macarrones</a></li>
+        </ul>
+      </div>
+    </div>
+  )
+  
+}
+
+export default Footer
