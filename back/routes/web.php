456de68a177fd753dc9e83e4a26fa309ad054d76
Beadeavila <beadeavila10@gmail.com>
2023-03-07 16:27:35 +0100

    fixing some remaning conflict

--- back/routes/web.php
+++ back/routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ItemController;
-use App\Http\Controllers\HomeController;
+
 
 /*
 |--------------------------------------------------------------------------
