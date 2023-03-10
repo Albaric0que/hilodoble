1c2dc6f4f688a71291ce6b83602492dfbca6538f
AlesiaCoder <alesia.nekolethal@gmail.com>
2023-03-10 08:35:05 +0100

    css added to footer

--- front/src/components/Footer/Footer.jsx
+++ front/src/components/Footer/Footer.jsx
@@ -8,18 +8,26 @@ function Footer() {
     <div className='footerContainer'>
       <div className='footerTop'>
         <div className='footerBlock'>
-
+          <p>HiloDoble es la oportunidad de compartir solidaridad llevando diseños con materiales reciclados y ofrecer un empleo a colectivos en vulnerabilidad.</p>
         </div>
         <div className='footerBlock'>
+          <h3>Nuestra web</h3>
           <ul>
-            <li><a href="#">macarrones</a></li>
-            <li><a href="#">macarrones</a></li>
-            <li><a href="#">macarrones</a></li>
-            <li><a href="#">macarrones</a></li>
+            <li><a href="#">Inicio</a></li>
+            <li><a href="#">Tienda</a></li>
+            <li><a href="#">Sobre nosotros</a></li>
+            <li><a href="#">Tiendas amigas</a></li>
+            <li><a href="#">Blog</a></li>
+            <li><a href="#">Contacta</a></li>
           </ul>
         </div>
         <div className='footerBlock'>
-
+          <p>Síguenos en:</p>
+          <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+          </ul>
         </div>
       </div>
       
