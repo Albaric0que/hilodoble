57d675d4cf3cd6402fb832bfdb81f2f6c2b7b687
Paloma Babot <palomababot@MacBook-Pro-de-Maria.local>
2023-03-13 12:50:26 +0100

    Shoping Cart functional MVP Done

--- back/resources/views/cart.blade.php
+++ back/resources/views/cart.blade.php
@@ -13,20 +13,23 @@
                         <th>Precio total</th>
                     </tr>
                 </thead>
+
+                <!-- Below we are iterating over the $cartItems variable and accessing the itemName, price, and quantity properties of each Item model associated with the current cart item using the item relationship. We are also calculating the total price of all cart items using the sum() method on the $cartItems collection, and multiplying the price and quantity properties of each Item model to get the total price for each cart item. -->
                 <tbody>
-                    @foreach ($cartItems as $item)
-                        <tr>
-                            <td>{{ $item->itemName }}</td>
-                            <td>{{ $item->price }}</td>
-                            <td>{{ $item->quantity }}</td>
-                            <td>${{ $item->price * $item->quantity }}</td>
-                        </tr>
-                    @endforeach
-                    <tr>
-                        <td colspan="3" class="text-right"><strong>Total:</strong></td>
-                        <td><strong>${{ $item->price * $item->quantity  }}</strong></td>
-                    </tr>
-                </tbody>
+                            @foreach ($cartItems as $cartItem)
+                                <tr>
+                                    <td>{{ $cartItem->item->itemName }}</td>
+                                    <td>{{ $cartItem->item->price }}</td>
+                                    <td>{{ $cartItem->quantity }}</td>
+                                    <td>${{ $cartItem->item->price * $cartItem->quantity }}</td>
+                                </tr>
+                            @endforeach
+                            <tr>
+                                <td colspan="3" class="text-right"><strong>Total:</strong></td>
+                                <td><strong>${{ $cartItems->sum(function ($cartItem) { return $cartItem->item->price * $cartItem->quantity; }) }}</strong></td>
+                            </tr>
+                    </tbody>
+
             </table>
             <div class="text-center">
                 <a href="/" class="btn btn-primary">Seguir comprando</a>
