680a7bf5c44a67fadf61f345a71d477d04b73a16
Beadeavila <beadeavila10@gmail.com>
2023-03-13 18:22:06 +0100

    searchIcon

--- front/src/components/Searchbar/Searchbar.jsx
+++ front/src/components/Searchbar/Searchbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import searchIcon from '../img/searchIcon.png';
+import './Searchbar.css';
 
 function Searchbar() {
     return (
-        <>
+        <div>
             <link rel="stylesheet" type="text/css" href="Searchbar.css" />
-        </>
+            <input className="searchBox"
+            type="search"/>
+        </div>
+        
     )
 }
 
