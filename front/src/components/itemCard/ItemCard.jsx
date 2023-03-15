a2512e8577329b4b97629632991cc1888d6949bd
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-03-15 15:42:17 +0100

    installed react-router-dom + axios + camelCase in css's

--- front/src/components/itemCard/ItemCard.jsx
+++ front/src/components/itemCard/ItemCard.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {Link} from 'react-router-dom'
+import axios from 'axios';
+
+/* import {Link} from 'react-router-dom' */
 import '../itemCard/ItemCard.css'
 
 function ItemCard() {
@@ -9,17 +11,17 @@ function ItemCard() {
   const [ item, setItem ] = useState([])
 
   useEffect(()=>{
-    fetch(`RUTAAPIOCHOMILLLLL${param.id}`)
-    .then ((res)=>res.json())
-    .then ((data)=>setItem(data))
+    axios.get(`http://127.0.0.1:8000/${param.id}`)
+      .then((response) => setItem(response.data))
+      .catch((error) => console.log(error))
   },[param.id])
 
   return (
     <div key={item.id} className= "container">
-      <div src={item.image} alt={item.itemName} className= "img-container"></div>
+      <div src={item.image} alt={item.itemName} className= "imgContainer"></div>
       <div>
-        <h3 className= "name-item">{item.itemName}</h3>
-        <h3 className= "price-item">{item.price}</h3>
+        <h3 className= "nameItem">{item.itemName}</h3>
+        <h3 className= "priceItem">{item.price}</h3>
         <button className= "cartBtn">Añadir al carrito</button>
       </div>
     </div>
