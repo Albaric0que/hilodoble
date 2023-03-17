fcd68b3e4b7c5fcad16443e007c9fb5f8c4bef76
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-03-17 08:03:30 +0100

    merged dev to header

--- front/src/App.js
+++ front/src/App.js
@@ -3,8 +3,8 @@ import './App.css';
 import Header from './components/Header/Header';
 
 function App() {
-  return (
-    <div className="App">
+  return ( 
+    <div className="App"> 
       <Header/>
     </div>
   );
