143392cdcb640c04e2151e0df5ba652e1f637d18
Beadeavila <beadeavila10@gmail.com>
2023-03-20 15:02:08 +0100

    cors

--- front/src/components/allItems/AllItems.jsx
+++ front/src/components/allItems/AllItems.jsx
@@ -1,8 +1,41 @@
 import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
+/* import { Link } from 'react-router-dom' */
 import axios from 'axios';
 import '../allItems/AllItems.css'
 
+const baseURL ='http://127.0.0.1:8000/api';
+
+export default function AllItems(){
+  const [items, setItems]= React.useState(null);
+
+  React.useState(() =>{
+    axios.get(baseURL).then((response) =>{
+      setItems(response.data);
+    });
+  }, []);
+
+  if(!items) return null;
+
+  return (
+    <div className='container'>
+      <h1>Nuestra tienda</h1>
+      <h3>Artesanía, reciclaje y diseños exclusivos</h3>
+      {items.map((item) => {
+        return (
+          <div key={item.id}>
+            <div src={item.image} alt={item.itemName} className='imgContainer'></div>
+            <div>
+              <h3 className='nameItem'>{item.itemName}</h3>
+              <h3 className='priceItem'>{item.price}</h3>
+              <button className='cartBtn'>Añadir al carrito</button>
+            </div>
+          </div>
+        ) 
+      })}  
+    </div>
+  );
+} 
+/* 
 function AllItems() {
 
   const [ items, setItems ] = useState([]);
@@ -36,6 +69,6 @@ function AllItems() {
       })}  
     </div>
   )
-}
+} 
 
-export default AllItems
+ export default AllItems  */
