abc5c005637fbfc5396e140e8ff5839f78294e89
Paloma Babot leon <paloma@MacBook-Pro-de-Paloma.local>
2023-03-21 08:28:49 +0100

    CorsGate

--- front/src/App.js
+++ front/src/App.js
@@ -1,24 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ItemCard from './components/itemCard/ItemCard';
+import ShowItem from './components/showItem/ShowItem';
+import AllItems from './components/allItems/AllItems.jsx';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/itemCard' element={<ItemCard/>}/>
+          <Route path='/showItem/:id' element={<ShowItem/>}/>
+          <Route path='/allItems' element={<AllItems/>}/>
+        </Routes>
+      </BrowserRouter> 
     </div>
   );
 }
---
+++ front/src/components/allItems/AllItems.jsx
@@ -0,0 +1,41 @@
+    import React, { useEffect, useState } from 'react'
+    /* import { Link } from 'react-router-dom' */
+    import axios from 'axios';
+    import '../allItems/AllItems.css'
+    
+    function AllItems() {
+
+    const [ items, setItems ] = useState([]);
+
+    useEffect(()=>{
+        axios.get(`http://127.0.0.1:8000/api`)
+        .then((response) =>  {
+            const data = response.data;
+            setItems(data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }, []);
+
+    return (
+        <div className='containerCard'>
+        <h1>Nuestra tienda</h1>
+        <h3>Artesanía, reciclaje y diseños exclusivos</h3>
+        {items.map((item) => {
+            return (
+            <div key={item.id}>
+                <img src={item.image} alt={item.itemName} className='imgContainer'></img>
+                <div>
+                <h3 className='nameItem'>{item.itemName}</h3>
+                <h3 className='priceItem'>{item.price}</h3>
+                <button className='cartBtn'>Añadir al carrito</button>
+                </div>
+            </div>
+            ) 
+        })}  
+        </div>
+    )
+    } 
+
+    export default AllItems  
