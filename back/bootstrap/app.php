12003689fda320aaf0ab8061356d548e6852cf95
Paloma <78784528+Uxoa@users.noreply.github.com>
2023-03-21 12:12:22 +0100

    Adding Cors Solution in Bootstrap/app.php

--- back/bootstrap/app.php
+++ back/bootstrap/app.php
@@ -4,7 +4,6 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: *');
 header('Access-Control-Allow-Headers: *');
 
-
 /*
 |--------------------------------------------------------------------------
 | Create The Application
