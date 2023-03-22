f2604f41320049a63475e3e1f50f622a0b1bef92
Esther <estherdlz@hotmail.com>
2023-03-22 09:32:04 +0100

    branch collabs2 updated

--- front/src/App.js
+++ front/src/App.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/headeer/Header';
+import Collabs from './components/collabs/Collabs'
+
+
 
 function App() {
   return ( 
     <div className="App"> 
       <Header/>
+      <Collabs/>     
+      
     </div>
   ); 
 }
