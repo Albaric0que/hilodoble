b5a6157c0a93f721cf7cd189f6470412f6d98d4b
Paloma Babot <palomababot@MacBook-Pro-de-Maria.local>
2023-03-25 14:41:48 +0100

    more cleaning files

---
+++ front/src/components/myProfile/MyProfile.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './MyProfile.css';
+
+function MyProfile() {
+    return (
+        <>
+            <a href={`/login`}>Login</a>
+        </>
+    )
+}
+
+export default MyProfile
