0feea6884f501af11412697f0b5172fbb63c64a3
Nataliaplm <nataliapalomof@gmail.com>
2023-03-26 15:39:40 +0200

    some changes to home

--- front/src/components/home/Home.jsx
+++ front/src/components/home/Home.jsx
@@ -1,10 +1,64 @@
-import React from 'react';
+/* import React from 'react'; */
+import React, { useEffect, useState } from 'react'
 import '../home/Home.css';
+import axios from 'axios';
+import { useParams } from 'react-router-dom'
 
-function Home() {
-  return (
-    <div className= 'homeTitle'>🏡HOLIIIII ESTA ES LA HOME!!!🏡</div>
-  )
-}
+/* function Home() {
+    return (
+        <div className= 'homeTitle'>🏡HOLIIIII ESTA ES LA HOME!!!🏡</div>
+    )
+} */
 
-export default Home
+const Home = () => {
+/*     const [sliderImages, setSliderImages] = useState([]);
+    const [featuredProducts, setFeaturedProducts] = useState([]); */
+    const param = useParams();
+    const [ item, setItem ] = useState([]);
+  
+    useEffect(() => {
+      // Obtener imágenes del slider
+      axios
+        .get(`http://127.0.0.1:8000/api/showItem/${param.id}`)
+        .then((response) => {
+          setItem(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });   
+  
+      // Obtener productos destacados
+      axios
+        .get("https://ejemplo.com/api/featured-products")
+        .then((response) => {
+          setItem(response.data);
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }, []);
+  
+    return (
+      <div>
+       {/*  {Slider} */}
+        <div className="slider">
+          {item.map((image) => (
+            <img key={item.id} src={item.url} alt={item.alt} />
+          ))}
+        </div>
+  
+        {/* Productos destacados */}
+        <div className="featured-products">
+          {item.map((product) => (
+            <div key={product.id}>
+              <h2>{product.title}</h2>
+              <p>{product.description}</p>
+              <img src={product.image} alt={product.title} />
+            </div>
+          ))}
+        </div>
+      </div>
+    );
+  };
+  
+  export default Home;
