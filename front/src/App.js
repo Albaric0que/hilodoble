bcaf8b1db6e5c9998e3b27fd8ca4b86fdfb4e115
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-03-28 14:06:53 +0200

    starting login auth

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import { BrowserRouter, Routes, Route, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/header/Header';
 import Collabs from './components/collabs/Collabs'
 import AboutUs from './components/aboutUs/AboutUs';
@@ -8,13 +8,35 @@ import ShowItem from './components/showItem/ShowItem';
 import AllItems from './components/allItems/AllItems';
 import CartUser from './components/cartUser/CartUser';
 import Footer from './components/Footer/Footer';
+import Button from './components/collabs/Button';
 
 
 function App() {
+
+  const [user, setUser] = useState(null)
+
+  const login = () => {
+    setUser ({
+      id: 1,
+      email: "femcoder@hilodoble.com"
+    })
+  }
+
+  const logout = () => setUser(null)
+
   return (
     <div className="App">
       <Header />
       <BrowserRouter>
+
+        {
+          user ? (
+          <button onClick={logout}>Logout</button>
+          ) : (
+            <button onClick={login}>Login</button>
+          )
+        }
+
         <Routes>
           <Route path='/showItem/:id' element={<ShowItem />} />
           <Route path='/allItems' element={<AllItems />} />
