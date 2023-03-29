bd2087d4e279558f7f11e15a4beee988548bf202
Nataliaplm <nataliapalomof@gmail.com>
2023-03-30 00:50:39 +0200

    header components added

--- front/src/components/header/Header.jsx
+++ front/src/components/header/Header.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 import logo from '../img/imgHeader/logo.png';
+import cart from '../img/imgHeader/cart.svg';
 import Head from './head/Head';
 import Navbar from './navbar/Navbar';
+import Searchbar from '../header/searchbar/Searchbar'
+import MyProfile from '../header/myProfile/MyProfile'
 import './Header.css'; 
 
 function Header() {
@@ -11,6 +14,11 @@ function Header() {
         <Head/>
         <div className='mainIcons'>
           <img className='logo' src={logo} alt="logotipo" />
+          <div className='rightIcons'>
+            <Searchbar/>
+            <img className='cart' src={cart} alt="logo" />
+            <MyProfile/>
+          </div>
           <div className='toggleMenu'>
             <Navbar/>
           </div>
