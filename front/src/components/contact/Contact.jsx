eb5e3abc758c11c898624df8f2d71aadccc7d35e
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-03-29 13:02:45 +0200

    fixed formspree - working on implementation - send button doesn't seem connected to email

--- front/src/components/contact/Contact.jsx
+++ front/src/components/contact/Contact.jsx
@@ -1,131 +1,12 @@
-import React, { useState } from 'react';
-import * as Yup from 'yup';
+import React from 'react';
 import { useForm, ValidationError } from '@formspree/react';
-import { Formik, Field, Form, ErrorMessage } from 'formik';
-import { FormGroup, Button, Alert, Row, Col } from 'reactstrap';
 import './Contact.css'
-import FormButton from "./FormButton";
+import FormButton from './FormButton';
 import contactImg from '../img/imgContact/contactImg.png';
 
 function Contact() {
     
-    const schema = Yup.object().shape({
-        name: Yup.string().required("Por favor, introduce tu nombre"),
-        lastName: Yup.string().required("Por favor, introduce tus apellidos"),
-        email: Yup.string().email("Introduce una dirección de correo electrónico válida").required("Por favor, introduce tu correo electrónico"),
-        message: Yup.string().required("Por favor, introduce un mensaje"),
-    });
-    
-    <Formik
-        initialValues={{
-            Name:'',
-            LastName:'',
-            Email:'',
-            Message:'',
-        }}
-        validationSchema={schema}
-        ></Formik>
-
-/*     const [state, handleSubmit] = useForm("xlekpege");
-    const [validationErrors, setValidationErrors] = useState({});
-    
-    const onSubmit = async (values) => {
-        try {
-            await schema.validate(values, { abortEarly: false });
-            setValidationErrors({});
-            handleSubmit(values);
-        } catch (err) {
-            if (err instanceof Yup.ValidationError) {
-                const errors = {};
-                err.inner.forEach((e) => {
-                    errors[e.path] = e.message;
-                });
-                setValidationErrors(errors);
-            }
-        }
-    }; */
-    
-    /* if (state.succeeded) {
-        return <p>¡Muchas gracias por su mensaje! Nos pondremos en contacto con lo antes posible.</p>;
-    } */
-    return (
-        <>
-            <form>
-                <FormGroup>
-                    <label htmlFor="name">Nombre:</label>
-                    <Field
-                        className="contactFormTxt"
-                        name="Name"
-                        placeholder="Pepe"
-                        type="text"
-                    />
-                    <ErrorMessage 
-                        name="Name"
-                        component="div"
-                        className="field-error text-danger"
-                    />
-                </FormGroup>
-
-                <FormGroup>
-                    <label htmlFor="lastName">Apellidos:</label>
-                    <Field
-                        className="contactFormTxt"
-                        name="LastName"
-                        placeholder="Gómez"
-                        type="text"
-                    />
-                    <ErrorMessage 
-                        name="LastName"
-                        component="div"
-                        className="field-error text-danger"
-                    />
-                </FormGroup>
-
-                <FormGroup>
-                    <label htmlFor="lastName">Correo Electrónico:</label>
-                    <Field
-                        className="contactFormTxt"
-                        name="Email"
-                        placeholder="nombre@hilodoble.com"
-                        type="email"
-                    />
-                    <ErrorMessage 
-                        name="Email"
-                        component="div"
-                        className="field-error text-danger"
-                    />
-                </FormGroup>
-
-                <FormGroup>
-                    <label htmlFor="lastName">Sugerencia o mensaje:</label>
-                    <Field
-                        className="contactFormTxt"
-                        name="Message"
-                        placeholder="Buenas tardes. Mi nombre es Femcoder y me encantaría..."
-                        type="textarea"
-                    />
-                    <ErrorMessage 
-                        name="Message"
-                        component="div"
-                        className="field-error text-danger"
-                    />
-                </FormGroup>
-                <Row>
-                    <Col lg={12} md={12}>
-                        <FormButton type="submit" />
-                    </Col>
-                </Row>
-        </form>
-
-        <div className="contactBanner">
-            <img src={contactImg} alt="Banner inferior de mujer con teléfono móvil" />
-        </div>
-        </>
-    );
-}
-
-
-    /* const [state, handleSubmit] = useForm("xlekpege");
+    const [state, handleSubmit] = useForm('xlekpege');
     if (state.succeeded) {
         return <p>¡Muchas gracias por su mensaje! Nos pondremos en contacto con lo antes posible.</p>;
     }
@@ -139,36 +20,71 @@ function Contact() {
             <div className='contentBox'>  
                 <p className='bodyText'>¿Tienes alguna duda, propuesta o sugerencia? Buscamos la respuesta indicada a tu mensaje, para ello contacta con nosotros o escríbenos mediante el formulario.</p>
                 <div className='contactFormBox'>
-                    <form action='https://formspree.io/f/xlekpege' method='POST'>
+                    <form onSubmit={handleSubmit}>
                         <div className='contactFormTopInfo'>
                             <div>
-                                <label className='contactFormTxt'>Nombre: </label>
-                                <input type="text" name="name" value={formData.name} onChange={handleInputChange} required/>
+                                <label htmlFor='name' className='contactFormTxt'>Nombre: </label>
+                                <input 
+                                    id='name' 
+                                    type='text'
+                                    name='name'
+                                />
+                                <ValidationError 
+                                    prefix='Name' 
+                                    field='name'
+                                    errors={state.errors}
+                                />
                             </div>
                             <div>
-                                <label className='contactFormTxt'>Apellidos: </label>
-                                <input type="text" name="surname" value={formData.surname} onChange={handleInputChange} required/>
+                                <label htmlFor='surname' className='contactFormTxt'>Apellidos: </label>
+                                <input 
+                                    id='surname' 
+                                    type='text'
+                                    name='surname'
+                                />
+                                <ValidationError 
+                                    prefix='Surname' 
+                                    field='surname'
+                                    errors={state.errors}
+                                />
                             </div>
                         </div>
 
                         <div>
-                            <label className='contactFormTxt'>Correo electrónico: </label>
-                            <input type="email" name="email" value={formData.email} onChange={handleInputChange} required/>
+                            <label htmlFor='email' className='contactFormTxt'>Correo electrónico: </label>
+                            <input 
+                                    id='email' 
+                                    type='email'
+                                    name='email'
+                                />
+                                <ValidationError 
+                                    prefix='Email' 
+                                    field='email'
+                                    errors={state.errors}
+                                />
                         </div>
 
                         <div>
-                            <label className='contactFormTxt'>Sugerencia o mensaje: </label>
-                            <textarea name="suggestionOrMessage" value={formData.suggestionOrMessage} onChange={handleInputChange} required/>
+                            <label htmlFor='message' className='contactFormTxt'>Sugerencia o mensaje: </label>
+                            <textarea
+                                id='message'
+                                name='message'
+                            />
+                            <ValidationError 
+                                prefix='Message' 
+                                field='message'
+                                errors={state.errors}
+                            />
                         </div>
                     </form>
-                    <FormButton />
+                    <FormButton type='submit' disabled={state.submitting} />
                 </div>
             </div>
-            <div className="contactBanner">
-                <img src={contactImg} alt="Banner inferior de mujer con teléfono móvil" />
+            <div className='contactBanner'>
+                <img src={contactImg} alt='Banner inferior de mujer con teléfono móvil' />
             </div>
         </>
     )
-} */
+}
 
 export default Contact
--- front/src/components/contact/FormButton.jsx
+++ front/src/components/contact/FormButton.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import './FormButton.css'
 
 function FormButton() {
@@ -10,65 +10,3 @@ function FormButton() {
 }
 
 export default FormButton
-
-
-
-import React from 'react';
-import { useForm, ValidationError } from '@formspree/react';
-import './Contact.css'
-import FormButton from "./FormButton";
-import contactImg from '../img/imgContact/contactImg.png';
-
-function Contact() {
-
-    const [state, handleSubmit] = useForm("xlekpege");
-    if (state.succeeded) {
-        return <p>¡Muchas gracias por su mensaje! Nos pondremos en contacto con lo antes posible.</p>;
-    }
-
-    return (
-        <>
-            
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="email">Nombre:</label>
-                <input id="name" type="text" name="name"/>
-                <ValidationError 
-                    prefix="Name" 
-                    field="name"
-                    errors={state.errors}
-                />
-
-                <label htmlFor="last_name">Apellidos:</label>
-                <input id="last_name" type="text" name="last_name" />
-                <ValidationError 
-                    prefix="Last Name" 
-                    field="last_name"
-                    errors={state.errors}
-                />
-
-                <label htmlFor="email">Correo electrónico:</label>
-                <input id="email" type="email" name="email" />
-                <ValidationError 
-                    prefix="Email" 
-                    field="email"
-                    errors={state.errors}
-                />
-
-                <label htmlFor="message">Sugerencia o mensaje:</label>
-                <textarea id="message" name="message"></textarea>
-                <ValidationError 
-                    prefix="Message" 
-                    field="message"
-                    errors={state.errors}
-                />
-
-                <FormButton type="submit" disabled={state.submitting} />
-            </form>
-                <div className="contactBanner">
-                    <img src={contactImg} alt="Banner inferior de mujer con teléfono móvil" />
-                </div>
-        </>
-    );
-    }
-
-
