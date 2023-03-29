25c58349c97975c7b80b0ed969234fffd9aa22aa
[mintybubblegum] <anamaria.tz@hotmail.com>
2023-03-29 12:06:21 +0200

    trying to implement nd validate form...no success yet

--- front/src/App.js
+++ front/src/App.js
@@ -13,7 +13,7 @@ import Footer from './components/Footer/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div className="App"> 
       <Header />
       <BrowserRouter>
         <Routes>
